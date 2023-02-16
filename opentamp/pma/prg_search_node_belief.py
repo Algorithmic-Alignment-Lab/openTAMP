8f57b602faacf21c927d81c0a2e7fa4d73a7506c
philljkc <pjkc99@gmail.com>
2023-02-16 14:53:44 -0500

    add signature methods to override from IBSP paper

--- opentamp/pma/prg_search_node_belief.py
+++ opentamp/pma/prg_search_node_belief.py
@@ -1,16 +1,64 @@
 from opentamp.core.internal_repr.state import State
 from opentamp.core.internal_repr.problem import Problem
 from opentamp.core.util_classes.learning import PostLearner
-from opentamp.pma.prg_search_node import HLSearchNode, LLSearchNode
+from opentamp.pma.prg_search_node import LLSearchNode
 
 import copy
 import functools
 import random
 
 
-class HLBeliefSearchNode(HLSearchNode):
-    pass
-
-
 class LLBeliefSeachNode(LLSearchNode):
-    pass
+    def __init__(self,
+                 domain,
+                 prob,
+                 initial,
+                 plan_str=None,
+                 plan=None,
+                 priority=1,
+                 keep_failed=False,
+                 ref_plan=None,
+                 expansions=0,
+                 tol=1e-3,
+                 refnode=None,
+                 freeze_ts=-1,
+                 hl=True,
+                 ref_traj=[],
+                 nodetype='optimal',
+                 env_state=None,
+                 label='',
+                 info={},
+                 x0=None,
+                 targets=None,
+                 debug=False):
+
+        super().__init__(domain,
+                 prob,
+                 initial,
+                 plan_str=None,
+                 plan=None,
+                 priority=1,
+                 keep_failed=False,
+                 ref_plan=None,
+                 expansions=0,
+                 tol=1e-3,
+                 refnode=None,
+                 freeze_ts=-1,
+                 hl=True,
+                 ref_traj=[],
+                 nodetype='optimal',
+                 env_state=None,
+                 label='',
+                 info={},
+                 x0=None,
+                 targets=None,
+                 debug=False)
+
+        def plan(self, solver, n_resamples=5, debug=False):
+            pass
+
+        def solved(self):
+            pass
+
+        def get_failed_pred(self, forward_only=False, st=0):
+            pass
