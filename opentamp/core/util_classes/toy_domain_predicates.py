74115b92f1754208936b78d9630ed9b83af063ad
philljkc <pjkc99@gmail.com>
2023-02-28 11:56:20 -0500

    complete skeletons for fuyll-observed toy domain

---
+++ opentamp/core/util_classes/toy_domain_predicates.py
@@ -0,0 +1,27 @@
+import sys
+import traceback
+
+import numpy as np
+
+from opentamp.core.internal_repr.predicate import Predicate
+
+class PointerAtTarget(Predicate):
+    def __init__(self, name, params, expected_param_types):
+        super().__init__(name, params, expected_param_types)
+
+    def test(self, time, negated=False, tol=None):
+        if not self.is_concrete():
+            return False
+
+        pass
+
+
+class PointerAtGoal(Predicate):
+    def __init__(self, name, params, expected_param_types):
+        super().__init__(name, params, expected_param_types)
+
+    def test(self, time, negated=False, tol=None):
+        if not self.is_concrete():
+            return False
+
+        pass
