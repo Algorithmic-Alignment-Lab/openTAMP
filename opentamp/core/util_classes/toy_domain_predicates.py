516d12af21ce10f4d791c61ee89dba6b6e5e3de1
philljkc <pjkc99@gmail.com>
2023-03-07 21:42:39 -0500

    changing range for values

--- opentamp/core/util_classes/toy_domain_predicates.py
+++ opentamp/core/util_classes/toy_domain_predicates.py
@@ -39,6 +39,7 @@ class PointerAtLocation(Predicate):
 
         return np.abs(value_vec[0].item() - value_vec[1].item()) < 0.01
 
+
 class AlwaysTrue(Predicate):
     def __init__(self,  name, params, expected_param_types, env=None, active_range=(0,0), priority = 0):
         super().__init__(name, params, expected_param_types)
