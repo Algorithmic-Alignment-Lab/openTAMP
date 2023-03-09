3d49ea58bdaae44b4e1f5bc68ec4a0274230400f
philljkc <pjkc99@gmail.com>
2023-03-08 19:11:20 -0500

    add ToySolver for low level optimization

---
+++ opentamp/pma/toy_solver.py
@@ -0,0 +1,20 @@
+# from opentamp.pma.backtrack_ll_solver_OSQP import *
+
+class ToySolver():
+    def __init__(self, sigma):
+        self.sigma = 0
+
+    # used for the ToyDomain demo
+    def _backtrack_solver(self,
+        plan,
+        callback=None,
+        anum=0,
+        verbose=False,
+        amax=None,
+        n_resamples=5,
+        init_traj=[],
+        st=0,
+        debug=False
+    ):
+        print(plan)
+        return True
