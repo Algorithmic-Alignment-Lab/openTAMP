20540bdcaca1d90e88968d026f8d77dde154748c
Olivia Siegel <oliviasiegel@Olivias-MacBook-Pro.local>
2023-03-11 16:37:19 -0500

    unindent

--- opentamp/bash_scripts/run_py3.sh
+++ opentamp/bash_scripts/run_py3.sh
@@ -1,33 +1,33 @@
 #for N in 1 2 3 4 5
 #do
-    for S in third
-    do
+for S in third
+do
 
 
-        python3 -W ignore policy_hooks/run_training.py -c policy_hooks.namo.hyperparams_v94 \
-                                                       -no 1 -llus 500  -hlus 500 \
-                                                       -spl -mask -hln 2 -hldim 96 -lldim 64 \
-                                                       -retime -vel 0.3 -eta 5 -softev \
-                                                       -lr_schedule adaptive \
-                                                       -obs_del -hist_len 2 -prim_first_wt 20 -lr 0.0002 \
-                                                       -hllr 0.001 -lldec 0.0001 -hldec 0.0004 \
-                                                       -add_noop 2 --permute_hl 1 \
-                                                       -expl_wt 10 -expl_eta 4 \
-                                                       -col_coeff 0.0 \
-                                                       -motion 5 \
-                                                       -n_gpu 2 \
-                                                       -rollout 1 \
-                                                       -task 1 \
-                                                       -post -pre \
-                                                       -warm 100 \
-                                                       -neg_ratio 0. -opt_ratio 0.7 -dagger_ratio 0.3 \
-                                                       -descr mac_test & 
-        sleep 10800 
-        pkill -f run_train -9
-        pkill -f ros -9
-        sleep 5
+    python3 -W ignore policy_hooks/run_training.py -c policy_hooks.namo.hyperparams_v94 \
+                                                    -no 1 -llus 500  -hlus 500 \
+                                                    -spl -mask -hln 2 -hldim 96 -lldim 64 \
+                                                    -retime -vel 0.3 -eta 5 -softev \
+                                                    -lr_schedule adaptive \
+                                                    -obs_del -hist_len 2 -prim_first_wt 20 -lr 0.0002 \
+                                                    -hllr 0.001 -lldec 0.0001 -hldec 0.0004 \
+                                                    -add_noop 2 --permute_hl 1 \
+                                                    -expl_wt 10 -expl_eta 4 \
+                                                    -col_coeff 0.0 \
+                                                    -motion 5 \
+                                                    -n_gpu 2 \
+                                                    -rollout 1 \
+                                                    -task 1 \
+                                                    -post -pre \
+                                                    -warm 100 \
+                                                    -neg_ratio 0. -opt_ratio 0.7 -dagger_ratio 0.3 \
+                                                    -descr mac_test & 
+    sleep 10800 
+    pkill -f run_train -9
+    pkill -f ros -9
+    sleep 5
 
 
-    done
+done
 #done
 
