188f9f44a8b75b08f8ac9a85c1f2dae43a7dfc19
philljkc <pjkc99@gmail.com>
2023-03-27 16:14:34 -0400

    revert to ToySolver with dummy overrides

--- opentamp/pma/toy_solver.py
+++ opentamp/pma/toy_solver.py
@@ -3,38 +3,19 @@ from sco_py.expr import AffExpr, BoundExpr, QuadExpr
 from sco_py.sco_osqp.prob import Prob
 from sco_py.sco_osqp.solver import Solver
 from sco_py.sco_osqp.variable import Variable
+from pma.backtrack_ll_solver_OSQP import BacktrackLLSolverOSQP
 
 import numpy as np
 import torch as
 
-class ToySolver():
+class ToySolver(BacktrackLLSolverOSQP):
     def __init__(self, sigma):
         self.sigma = sigma
 
     # used for the ToyDomain demo, just randomize around the proposed location
     # with variation sigma
-    def _backtrack_solve(self,
-        plan,
-        callback=None,
-        anum=0,
-        verbose=False,
-        amax=None,
-        n_resamples=5,
-        init_traj=[],
-        st=0,
-        debug=False
-    ):
-        belief_state = plan.actions[0].params[0].value
+    def get_resample_param(self, a):
+        return None
 
-        # for now, just verifies if a select location
-        for act in plan.actions:
-            # only nontrivial action is observation plan
-            if act.name == 'observe':
-                prob = Prob()
-
-                mean_location = act.params[0].value[0].item()
-                set_location = act.params[1].value.item()
-                if np.abs(set_location - mean_location) < 0.01:
-                    return True
-
-        return False
+    def obj_pose_suggester(self, plan, anum, resample_size=1, st=0):
+        return None
