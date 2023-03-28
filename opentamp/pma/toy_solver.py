2645c96e420297eff3b422684e81a6d1bb5103a6
philljkc <pjkc99@gmail.com>
2023-03-28 16:07:52 -0400

    mod solver

--- opentamp/pma/toy_solver.py
+++ opentamp/pma/toy_solver.py
@@ -10,8 +10,9 @@ import torch
 
 class ToySolver(BacktrackLLSolverOSQP):
     def get_resample_param(self, a):
+        print(a)
         return a.params[1]
 
     def obj_pose_suggester(self, plan, anum, resample_size=1, st=0):
-        print({"value": plan.params['g'].value})
-        return [{"value": plan.params['g'].value}]
+        print({"value": plan.params['p1'].value})
+        return [{"value": plan.params['p1'].value}]
