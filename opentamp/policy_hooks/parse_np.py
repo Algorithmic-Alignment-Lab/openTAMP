81dc7bbc7377ffb4f4dcc37bf3aa0ba3c19b59aa
Olivia Siegel <oliviasiegel@31-38-246.wireless.csail.mit.edu>
2023-04-12 14:04:12 -0400

    parse

--- opentamp/policy_hooks/parse_np.py
+++ opentamp/policy_hooks/parse_np.py
@@ -12,7 +12,7 @@ for pts in data:
     no, nt = int(pt[4]), int(pt[5])
     #if label.find('cereal-and-milk') >= 0 and pt[10] > 1400: continue
     #pt[1] = pt[1] * 20 # Path len
-    data_list.append({'time': pt[3], 'success at end': pt[0], 'path length': 20. * pt[1], 'distance from goal': pt[2], 'n_data': pt[6], 'key': (no, nt), 'label': label, 'ind': i, 'success anywhere': pt[7], 'optimal_rollout_success': pt[9], 'number of plans': pt[10], 'subgoals anywhere': pt[11], 'subgoals closest distance': pt[12], 'collision': pt[8], 'exp id': i})
+    data_list.append({'time': pt[3], 'success at end': pt[0], 'path length': 20. * pt[1], 'distance from goal': pt[2], 'n_data': pt[6], 'key': (no, nt), 'ind': i, 'success anywhere': pt[7], 'optimal_rollout_success': pt[9], 'number of plans': pt[10], 'subgoals anywhere': pt[11], 'subgoals closest distance': pt[12], 'collision': pt[8], 'exp id': i})
     # all_data[k][full_exp][cur_dir][cur_dir].append({'time': pt[3], 'success at end': pt[0], 'path length': 20. * pt[1], 'distance from goal': pt[2], 'n_data': pt[6], 'key': (no, nt), 'label': label, 'ind': i, 'success anywhere': pt[7], 'optimal_rollout_success': pt[9], 'number of plans': pt[10], 'subgoals anywhere': pt[11], 'subgoals closest distance': pt[12], 'collision': pt[8], 'exp id': i})
     if len(pt) > 13:
         data_list[-1]['any target'] = pt[13]
