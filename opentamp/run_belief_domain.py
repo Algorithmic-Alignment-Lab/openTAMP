1a613e26f542611c73e9694c5d5ff3ae28d77814
philljkc <pjkc99@gmail.com>
2023-04-14 02:05:56 -0400

    remove prints

--- opentamp/run_belief_domain.py
+++ opentamp/run_belief_domain.py
@@ -53,7 +53,6 @@ def toy_observation(plan):
     obs = torch.torch.empty(len(plan))
     for i in range(len(plan)):
         # differentially take conditional depending on the ray
-        print(plan[i].pose[0][1+i])
         if is_in_ray(plan[i].pose[0][1+i], belief.item()):
             obs[i] = pyro.sample('obs'+str(i), dist.Uniform(0.49, 0.51))
         else:
