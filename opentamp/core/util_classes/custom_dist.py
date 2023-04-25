73638486a73b6a06f3695a6ed71e5eee67a3743e
philljkc <pjkc99@gmail.com>
2023-04-25 10:26:13 -0400

    add torch.tensor 3

--- opentamp/core/util_classes/custom_dist.py
+++ opentamp/core/util_classes/custom_dist.py
@@ -7,15 +7,15 @@ import torch.distributions.constraints as constraints
 class CustomDist(Distribution):
     def __init__(self, sample_data):
         self.sample_data = sample_data
+        print(self.sample_data)
         self.support = constraints.real_vector
 
     def sample(self):
         rand_idx = torch.randint(high=self.sample_data.shape[0], size=(1,))
-        return torch.tensor(self.sample_data[rand_idx.item()])
+        return torch.tensor(self.sample_data[rand_idx])
 
     def log_prob(self, x):
-        equalities = torch.norm(x -  self.sample_data, p=2) < 0.001
-        print(-torch.log(torch.tensor(self.sample_data.shape[0])))
+        equalities = torch.norm(x - torch.tensor(self.sample_data), p=2) < 0.001
         if equalities.any():  # if is a sample
             return -torch.log(torch.tensor(self.sample_data.shape[0]))
         else:
