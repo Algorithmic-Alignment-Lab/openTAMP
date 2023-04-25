f897ab2371cf7f2a515ae4c5d39dc9fe54e4f8ef
philljkc <pjkc99@gmail.com>
2023-04-25 11:12:48 -0400

    rewrite with probabilities

--- opentamp/run_belief_domain.py
+++ opentamp/run_belief_domain.py
@@ -52,7 +52,7 @@ def toy_observation(rs_params, plan_belief_samples):
 
     import pdb; pdb.set_trace()
 
-    b_global_idx = pyro.sample('belief_global_idx', dist.Categorical(logits=torch.ones(1000)))
+    b_global_idx = pyro.sample('belief_global_idx', dist.Categorical(torch.ones(1000)/1000))
     b_global = pyro.param('belief_global', plan_belief_samples[b_global_idx])
 
     if rs_params is None:
