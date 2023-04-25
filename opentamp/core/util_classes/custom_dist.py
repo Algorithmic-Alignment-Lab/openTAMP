2c405366e16dd22a1282c58100ce64aa5881af67
philljkc <pjkc99@gmail.com>
2023-04-25 10:24:53 -0400

    add torch.tensor 2

--- opentamp/core/util_classes/custom_dist.py
+++ opentamp/core/util_classes/custom_dist.py
@@ -11,8 +11,7 @@ class CustomDist(Distribution):
 
     def sample(self):
         rand_idx = torch.randint(high=self.sample_data.shape[0], size=(1,))
-        print(self.sample_data[rand_idx.item()])
-        return self.sample_data[rand_idx.item()]
+        return torch.tensor(self.sample_data[rand_idx.item()])
 
     def log_prob(self, x):
         equalities = torch.norm(x -  self.sample_data, p=2) < 0.001
