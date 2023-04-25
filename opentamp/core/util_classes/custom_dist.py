54c741d46d95bcbe5d909f0c6e4c9e13a0d48ba3
philljkc <pjkc99@gmail.com>
2023-04-25 10:36:50 -0400

    requires_grad checks

--- opentamp/core/util_classes/custom_dist.py
+++ opentamp/core/util_classes/custom_dist.py
@@ -11,10 +11,13 @@ class CustomDist(Distribution):
 
     def sample(self):
         rand_idx = torch.randint(high=self.sample_data.shape[0], size=(1,))
-        return torch.tensor(self.sample_data[rand_idx])
+        return self.sample_data[rand_idx]
 
     def log_prob(self, x, *args, **kwargs):
         print(x)
         print(x-self.sample_data)
+        print(x.requires_grad)
+        print(self.sample_data.requires_grad)
         equalities = torch.norm(x - self.sample_data, p=2) < 0.001
+        print(equalities.requires_grad)
         return equalities.sum().log()
