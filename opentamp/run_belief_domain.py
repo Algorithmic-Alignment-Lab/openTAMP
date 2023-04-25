fd10c66e083d543848b2ab5fd9f65d2d8d1713c1
philljkc <pjkc99@gmail.com>
2023-04-25 11:36:07 -0400

    add prints

--- opentamp/run_belief_domain.py
+++ opentamp/run_belief_domain.py
@@ -53,6 +53,7 @@ def toy_observation(rs_params, plan_belief_samples):
     b_global_idx = pyro.sample('belief_global_idx', dist.Categorical(torch.ones(1000)/1000))
     b_global = pyro.param('belief_global', plan_belief_samples[b_global_idx])
 
+    print(torch.ones(1000)/1000)
     print(b_global)
 
     if rs_params is None:
