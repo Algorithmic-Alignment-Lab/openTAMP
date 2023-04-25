35ed5350891655eaa0f66576116dffddf8eeb8df
philljkc <pjkc99@gmail.com>
2023-04-25 18:07:48 -0400

    debug erroneous conditioning 2

--- opentamp/run_belief_domain.py
+++ opentamp/run_belief_domain.py
@@ -61,9 +61,11 @@ def toy_observation(rs_params, belief_mean, belief_cov):
             # differentially take conditional depending on the ray
             # 1.10714871779
             if is_in_ray(a.pose[0][i], b_global.item()):
-                obs[i-1] = pyro.sample('obs'+str(i), dist.Uniform(b_global.item()-0.001, b_global.item()+0.001))
+                tmp_obs = pyro.sample('obs'+str(i), dist.Uniform(b_global.item()-0.001, b_global.item()+0.001))
+                obs[i-1] = tmp_obs.item()
             else:
-                obs[i-1] = pyro.sample('obs'+str(i), dist.Uniform(b_global.item()-1, b_global.item()+1))  # no marginal information gotten
+                tmp_obs = pyro.sample('obs'+str(i), dist.Uniform(b_global.item()-1, b_global.item()+1))  # no marginal information gotten
+                obs[i-1] = tmp_obs.item()
 
     return obs
 
