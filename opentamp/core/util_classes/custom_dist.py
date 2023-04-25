6f08aba1ca895e1a9dae0deec7033c5cd07528e9
philljkc <pjkc99@gmail.com>
2023-04-25 10:43:06 -0400

    add no_grad

--- opentamp/core/util_classes/custom_dist.py
+++ opentamp/core/util_classes/custom_dist.py
@@ -10,14 +10,11 @@ class CustomDist(Distribution):
         self.support = constraints.real_vector
 
     def sample(self):
-        rand_idx = torch.randint(high=self.sample_data.shape[0], size=(1,))
-        return self.sample_data[rand_idx]
+        with torch.no_grad():
+            rand_idx = torch.randint(high=self.sample_data.shape[0], size=(1,))
+            return self.sample_data[rand_idx]
 
     def log_prob(self, x, *args, **kwargs):
-        print(x)
-        print(x-self.sample_data)
-        print(x.requires_grad)
-        print(self.sample_data.requires_grad)
-        equalities = torch.norm(x - self.sample_data, p=2) < 0.001
-        print(equalities.requires_grad)
-        return equalities.sum().log()
+        with torch.no_grad():
+            equalities = torch.norm(x - self.sample_data, p=2) < 0.001
+            return equalities.sum().log()
