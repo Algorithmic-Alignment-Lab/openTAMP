c19f82ef0c603a051b098de963e99e67ca1d4316
philljkc <pjkc99@gmail.com>
2023-04-25 17:19:17 -0400

    add mean + cov prints

--- opentamp/run_belief_domain.py
+++ opentamp/run_belief_domain.py
@@ -50,6 +50,9 @@ def is_in_ray(item, belief):
 def toy_observation(rs_params, belief_mean, belief_cov):
     # uniformly randomly sample on the seen beliefs so far
 
+    print(belief_mean)
+    print(belief_cov)
+
     # b_global_idx = pyro.sample('belief_global_idx', dist.Categorical(torch.ones(1000)/1000))
     # b_global = pyro.param('belief_global', plan_belief_samples[b_global_idx])
 
