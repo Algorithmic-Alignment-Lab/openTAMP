8eece6909785793d41cc1b7233109eea6541342f
philljkc <pjkc99@gmail.com>
2023-04-25 18:40:48 -0400

    if name is main hack

--- opentamp/run_belief_domain.py
+++ opentamp/run_belief_domain.py
@@ -26,61 +26,62 @@ from core.util_classes.custom_dist import CustomDist
 import pyro.poutine as poutine
 from pyro.infer import MCMC, NUTS
 
-# TODO: initialize calls to planner, add paths to relevant folders
-domain_fname = os.getcwd() + "/opentamp/domains/belief_space_domain/toy_belief.domain"
-prob = os.getcwd() + "/opentamp/domains/belief_space_domain/probs/toy_belief.prob"
+if __name__ == '__main__':
+    # TODO: initialize calls to planner, add paths to relevant folders
+    domain_fname = os.getcwd() + "/opentamp/domains/belief_space_domain/toy_belief.domain"
+    prob = os.getcwd() + "/opentamp/domains/belief_space_domain/probs/toy_belief.prob"
 
-# configuring task plan
-d_c = main.parse_file_to_dict(domain_fname)
-domain = parse_domain_config.ParseDomainConfig.parse(d_c)
-hls = FFSolver(d_c)
+    # configuring task plan
+    d_c = main.parse_file_to_dict(domain_fname)
+    domain = parse_domain_config.ParseDomainConfig.parse(d_c)
+    hls = FFSolver(d_c)
 
-# configuing motion plan
-p_c = main.parse_file_to_dict(prob)
-problem = parse_problem_config.ParseProblemConfig.parse(p_c, domain, None, use_tf=True, sess=None, visual=False)
-solver = ToySolver()
-
-
-# TODO: do this with the implemented geom logic, do belief space in similar way
-def is_in_ray(item, belief):
-    return np.pi/2 - 0.1/2 - np.arctan(belief/1.0) <= item <= np.pi/2 + 0.1/2 - np.arctan(belief/1.0) and np.arctan(1) <= item <= np.pi - np.arctan(1)
+    # configuing motion plan
+    p_c = main.parse_file_to_dict(prob)
+    problem = parse_problem_config.ParseProblemConfig.parse(p_c, domain, None, use_tf=True, sess=None, visual=False)
+    solver = ToySolver()
 
 
-# NOTE: expected names for pyro samples are "belief_"{param-name}+""
-def toy_observation(rs_params, belief_mean, belief_cov):
-    # LaPlace estimate: todo SVI if needed
-    b_global = pyro.sample('belief_global', dist.Normal(belief_mean, belief_cov))
+    # TODO: do this with the implemented geom logic, do belief space in similar way
+    def is_in_ray(item, belief):
+        return np.pi/2 - 0.1/2 - np.arctan(belief/1.0) <= item <= np.pi/2 + 0.1/2 - np.arctan(belief/1.0) and np.arctan(1) <= item <= np.pi - np.arctan(1)
 
-    if rs_params is None:
-        return b_global
 
-    # start observations in the first action todo: loop this over actions in the plan
-    obs = torch.torch.empty(rs_params[0].pose.shape[1]-1)
-    for a in rs_params:
-        for i in range(1, rs_params[0].pose.shape[1]):
-            # differentially take conditional depending on the ray
-            # 1.10714871779
-            if is_in_ray(a.pose[0][i], b_global.item()):
-                obs[i - 1] = pyro.sample('obs'+str(i), dist.Uniform(b_global.item()-0.001, b_global.item()+0.001))
-            else:
-                obs[i - 1] = pyro.sample('obs'+str(i), dist.Uniform(b_global.item()-1, b_global.item()+1))  # no marginal information gotten
+    # NOTE: expected names for pyro samples are "belief_"{param-name}+""
+    def toy_observation(rs_params, belief_mean, belief_cov):
+        # LaPlace estimate: todo SVI if needed
+        b_global = pyro.sample('belief_global', dist.Normal(belief_mean, belief_cov))
 
-    return obs
+        if rs_params is None:
+            return b_global
 
-# Run planning to obtain a final plan.
-plan, descr = p_mod_abs(
-    hls, solver, domain, problem,
-    goal=None, observation_model=toy_observation, max_likelihood_obs=0.5, debug=False, n_resamples=10
-)
+        # start observations in the first action todo: loop this over actions in the plan
+        obs = torch.torch.empty(rs_params[0].pose.shape[1]-1)
+        for a in rs_params:
+            for i in range(1, rs_params[0].pose.shape[1]):
+                # differentially take conditional depending on the ray
+                # 1.10714871779
+                if is_in_ray(a.pose[0][i], b_global.item()):
+                    obs[i - 1] = pyro.sample('obs'+str(i), dist.Uniform(b_global.item()-0.001, b_global.item()+0.001))
+                else:
+                    obs[i - 1] = pyro.sample('obs'+str(i), dist.Uniform(b_global.item()-1, b_global.item()+1))  # no marginal information gotten
 
-if plan is not None:
-    print(plan.actions)
-    print(plan.params['theta'].pose)  # track pose through time
-    print(plan.params['g'].value)  # track goal through time (not modified)
-    print(plan.params['g'].belief.samples)
+        return obs
 
-print(descr)
+    # Run planning to obtain a final plan.
+    plan, descr = p_mod_abs(
+        hls, solver, domain, problem,
+        goal=None, observation_model=toy_observation, max_likelihood_obs=0.5, debug=False, n_resamples=10
+    )
 
-# TODO: implement replan logic when belief-space implemented
+    if plan is not None:
+        print(plan.actions)
+        print(plan.params['theta'].pose)  # track pose through time
+        print(plan.params['g'].value)  # track goal through time (not modified)
+        print(plan.params['g'].belief.samples)
 
-# TODO: BCheckSuccess (first pass implement, gets at needed subroutines)
+    print(descr)
+
+    # TODO: implement replan logic when belief-space implemented
+
+    # TODO: BCheckSuccess (first pass implement, gets at needed subroutines)
