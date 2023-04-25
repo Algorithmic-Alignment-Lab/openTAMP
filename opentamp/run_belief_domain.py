621e5f6bdde3b73b9b7143725ed2449d600e2eb9
philljkc <pjkc99@gmail.com>
2023-04-24 23:17:54 -0400

    add print 3

--- opentamp/run_belief_domain.py
+++ opentamp/run_belief_domain.py
@@ -49,7 +49,7 @@ def toy_observation(plan_belief):
     def belief_prog(rs_params):
         # uniformly randomly sample on the seen so far
         print(plan_belief.samples[0])
-        print(plan_belief.samples.shape[0])
+        print(plan_belief.samples.shape)
 
         belief = pyro.sample('belief_global', dist.Empirical(plan_belief.samples, torch.ones(plan_belief.samples.shape[0])))
 
