64a729b7f2370f711a1c18a9dd25fb0177c4c054
philljkc <pjkc99@gmail.com>
2023-04-24 23:02:40 -0400

    as rs_params prints

--- opentamp/run_belief_domain.py
+++ opentamp/run_belief_domain.py
@@ -49,6 +49,8 @@ def toy_observation(plan_belief):
     def belief_prog(rs_params):
         # uniformly randomly sample on the seen so far
 
+        print(rs_params)
+
         belief = pyro.sample('belief_global', dist.Empirical(plan_belief.samples, torch.ones(len(plan_belief.samples))))
 
         print(plan_belief.samples.shape)
