5e56d6683412c398f119ba7009fcf258166f2f78
philljkc <pjkc99@gmail.com>
2023-05-05 16:47:58 -0400

    add brownian motion sampler

---
+++ opentamp/belief_space_scripts/brownian_sample.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# samples a brownian motion of a given drift and volatility, given sampling at timesteps 
+def brownian_motion(timesteps, drift, vol):
+    increments = np.concatenate([0,np.ediff1d(timesteps)])
+    means = drift * np.ones(len(timesteps))
+    cov = np.diag(vol * increments)  # variances of Gaussians
+    gaus_sample = np.random.multivariate_normal(means, cov)
+    return np.cumsum(gaus_sample)
+    
