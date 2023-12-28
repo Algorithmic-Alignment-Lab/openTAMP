4a622288e3eba9ab74c72e31c99432554bf8781b
philljkc <pjkc99@gmail.com>
2023-12-28 11:43:35 -0800

    add past_task_enum and past_targ enum to observation

--- opentamp/policy_hooks/namo/new_env_hyperparam.py
+++ opentamp/policy_hooks/namo/new_env_hyperparam.py
@@ -88,6 +88,7 @@ def refresh_config(no=NUM_OBJS, nt=NUM_TARGS):
                         utils.MJC_SENSOR_ENUM,
                         # utils.PAST_TARG_ENUM,
                         utils.TASK_ENUM,
+                        utils.PAST_TASK_ENUM,
                         # utils.PAST_POINT_ENUM,
                         # utils.ONEHOT_GOAL_ENUM
                         # utils.TASK_ENUM,
