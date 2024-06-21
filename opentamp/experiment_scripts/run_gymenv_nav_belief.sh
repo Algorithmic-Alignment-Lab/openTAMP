967e7ee60c681763e80f8297984ae322a112c68f
philljkc <pjkc99@gmail.com>
2024-06-21 16:02:08 -0400

    smaller observations for obstacle avoidance

--- opentamp/experiment_scripts/run_gymenv_nav_belief.sh
+++ opentamp/experiment_scripts/run_gymenv_nav_belief.sh
@@ -7,7 +7,7 @@
 
 # runs the main job
 
-TRIAL_NAME='nav_dettrial_fullobs_bimodalwideavoid_medinit'
+TRIAL_NAME='nav_dettrial_fullobs_bimodalwideavoid_highimit_smallobs'
 
 python3 -W ignore policy_hooks/run_training.py -c new_specs.nav_domain_belief_dettest.new_env_hyperparam \
                                                 -no 1 -llus 1000  -hlus 1000 \
